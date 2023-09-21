cf7bec40c478f8abd5e5e2ae6f922bc18645dfdd
SebastianKrzosek <sbkxp98@gmail.com>
2023-09-21 20:15:41 +0200

    Update Create View

--- LeaveManagement.Web/Models/LeaveTypeVM.cs
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -7,8 +7,12 @@ namespace LeaveManagement.Web.Models
     {
         public int Id { get; set; }
         [Display(Name = "Leave Type Name")]
+        [Required]
         public string Name { get; set; }
+
         [Display(Name = "Default numbers of days")]
+        [Required]
+        [Range(1, 25, ErrorMessage = "Please enter a valid number")]
         public int DefaultDays { get; set; }
     }
 }
