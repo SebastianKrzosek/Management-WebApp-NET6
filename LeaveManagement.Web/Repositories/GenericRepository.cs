2b9e022cb00cd51e44c1b6a694987d16af45df30
SebastianKrzosek <sbkxp98@gmail.com>
2023-09-24 17:04:51 +0200

    Change using context in controller to Repository

--- LeaveManagement.Web/Repositories/GenericRepository.cs
+++ LeaveManagement.Web/Repositories/GenericRepository.cs
@@ -7,7 +7,6 @@ namespace LeaveManagement.Web.Repositories
     public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
         private readonly ApplicationDbContext context;
-
         public GenericRepository(ApplicationDbContext context)
         {
             this.context = context;
@@ -30,23 +29,23 @@ namespace LeaveManagement.Web.Repositories
                 throw new Exception("Can't find a row to delete.");
             }
         }
-
         public async Task<bool> Exists(int id)
         {
             var entity = await GetAsync(id);
             return entity != null;
         }
-
         public async Task<List<T>> GetAllAsync()
         {
             return await context.Set<T>().ToListAsync();
         }
-
-        public async Task<T> GetAsync(int id)
+        public async Task<T?> GetAsync(int? id)
         {
+            if (id == null)
+            {
+                return null;
+            }
             return await context.Set<T>().FindAsync(id);
         }
-
         public async Task UpdateAsync(T entity)
         {
             //context.Entry(entity).State = EntityState.Modified;
